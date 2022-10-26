edbe7982cb6da086ad9f40f125c90e31c05f66c4
Sergey Nikiforov <toozox@gmail.com>
2022-10-26 14:52:58 +0500

    Add running junkins from API

---
+++ run_jenkins.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl http://user:113ae610026b542b20c24767fb1be046b3@localhost:8080//job/run%20from%20API/build?token=hisaotehisotehisatehisthlrpcysotnhisonethisaonethuaoeuhrastuihasoethsatoehiaoei
